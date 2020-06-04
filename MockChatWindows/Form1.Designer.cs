fc8b9b4a229c91924e916477bdbc5fba201e5ad8
Christopher DeRoche <46451756+compact-disc@users.noreply.github.com>
2020-06-03 22:49:45 -0400

    changed border style

--- MockChatWindows/Form1.Designer.cs
+++ MockChatWindows/Form1.Designer.cs
@@ -82,6 +82,7 @@
             this.Controls.Add(this.mockButton);
             this.Controls.Add(this.textEntryBox);
             this.Controls.Add(this.mockTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
